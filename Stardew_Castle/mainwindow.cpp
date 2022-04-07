ef9841c7ecc8489a738bc387c4717c7225ef17ee
James <20250541@studentmail.ul.ie>
2022-04-07 16:31:19 +0100

    Simple GUI added

--- Stardew_Castle/mainwindow.cpp
+++ Stardew_Castle/mainwindow.cpp
@@ -6,10 +6,35 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->textEdit->setText("Many moons ago, Stardew Valley was a prosporous place for all.                     "
+                       "This all changed when a wicked witch placed a curse upon the kingdom.                 "
+                       "It changed the villagers and royal family into zombies!!                              ");
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->textEdit->setText("You have woken up in this distant castle miles away from your home town.           "
+                          "Your quest is to survive the night and escape!");
+
+
+}
+
+
+void MainWindow::on_textEdit_textChanged()
+{
+
+    on_pushButton_clicked();
+    {
+        ui->textEdit->setText("Please click a direction to go!");
+
+    }
+}
+
 
